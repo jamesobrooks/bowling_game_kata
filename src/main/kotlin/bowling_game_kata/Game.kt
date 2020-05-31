a77fe139137835c0d1677b56b10523f2c3885e8f
oran0007 <12275865+oran0007@users.noreply.github.com>
2020-05-30 19:52:55 -0500

    slide 42

--- src/main/kotlin/bowling_game_kata/Game.kt
+++ src/main/kotlin/bowling_game_kata/Game.kt
@@ -11,8 +11,13 @@ class Game {
         var score = 0
         var i = 0
         for (frame in 0..9) {
-            score += rolls[i] + rolls[i+1]
-            i += 2
+            if (rolls[i] + rolls[i+1] == 10) { // spare
+                score += 10 + rolls[i+2]
+                i += 2
+            } else {
+                score += rolls[i] + rolls[i+1]
+                i += 2
+            }
         }
         return score
     }

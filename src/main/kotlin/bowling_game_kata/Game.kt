fbe601be19a026bb8e12bd62a8af74c0e761aaf3
oran0007 <12275865+oran0007@users.noreply.github.com>
2020-05-30 20:07:08 -0500

    slide 48

--- src/main/kotlin/bowling_game_kata/Game.kt
+++ src/main/kotlin/bowling_game_kata/Game.kt
@@ -11,16 +11,25 @@ class Game {
         var score = 0
         var frameIndex = 0
         for (frame in 0..9) {
-            if (isSpare(frameIndex)) {
-                score += 10 + rolls[frameIndex+2]
-                frameIndex += 2
-            } else {
-                score += rolls[frameIndex] + rolls[frameIndex+1]
-                frameIndex += 2
+            when {
+                isStrike(frameIndex) -> {
+                    score += 10 + rolls[frameIndex+1] + rolls[frameIndex+2]
+                    frameIndex++
+                }
+                isSpare(frameIndex) -> {
+                    score += 10 + rolls[frameIndex+2]
+                    frameIndex += 2
+                }
+                else -> {
+                    score += rolls[frameIndex] + rolls[frameIndex+1]
+                    frameIndex += 2
+                }
             }
         }
         return score
     }
+
+    private fun isStrike(frameIndex: Int) = rolls[frameIndex] == 10
 
     private fun isSpare(frameIndex: Int) = rolls[frameIndex] + rolls[frameIndex + 1] == 10
 }
